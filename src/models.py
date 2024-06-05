2a665b7f262f526c9b34dbb5737ec9fdd69390d3
Gabriela Garin <gabriela.garin@gmail.com>
2024-06-05 23:19:04 +0000

    models.py ok

--- src/models.py
+++ src/models.py
@@ -7,23 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Admin(Base):
+    __tablename__ = 'admin'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    admin_id = Column(Integer, ForeignKey('admin.id'))
+    admin = relationship(Admin)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    user_favorites = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    media_id = Column(Integer, ForeignKey('media.id'))
+    media = relationship(Media)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    media_id = Column(Integer, ForeignKey('media.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    media = relationship(Media)
 
     def to_dict(self):
         return {}
